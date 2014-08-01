8780f093720ed361b7b94e90253c192485d6215c
Oggal <legoboy1122@yahoo.com>
2014-08-01 17:36:44 -0500

    Started work on a start menu

---
+++ GrapeSlush/Assets/scripts/MainMenu/StartButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton : MonoBehaviour {
+	float deltaRot = 0;
+	public float speed = 10f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//transform.Rotate (Vector3.up, 100f * Time.deltaTime);		120,20,120
+		float rot = speed * Time.deltaTime;
+		transform.Rotate(0,rot,0);
+		deltaRot = deltaRot + rot;
+		if (deltaRot >= 89f) {
+			deltaRot = -89f;
+			transform.rotation = Quaternion.Euler(0,-89,0);
+		}
+	}
+}
---
+++ GrapeSlush/Assets/scripts/Networking/NetworkManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	SpawnSpot[] Spawns;
+	public GameObject StandbyCamera;
+	public string Version = "07312014-02.10_dev";
+	// Use this for initialization
+	void Start () {
+		Spawns = GameObject.FindObjectsOfType<SpawnSpot>();
+		Connect ();
+
+	}
+
+	void Connect () {
+		PhotonNetwork.ConnectUsingSettings("GrapeSlush"+Version);
+	}
+
+	void OnGUI(){
+		GUILayout.Label ( PhotonNetwork.connectionStateDetailed.ToString ());
+	}
+
+	void OnJoinedLobby(){
+		PhotonNetwork.JoinRandomRoom ();
+	}
+
+	void OnPhotonRandomJoinFailed(){
+		Debug.Log ("Join Random Room Failed!");
+		PhotonNetwork.CreateRoom (null);
+	}
+
+	void OnJoinedRoom(){
+		Debug.Log ("Joined Room");
+		SpawnClientPlayer ();
+	}
+
+	void SpawnClientPlayer(){
+		Debug.Log ("Player Spawned");
+
+		SpawnSpot SpawnPoint = Spawns [Random.Range (0, Spawns.Length)];
+		GameObject MyPly =PhotonNetwork.Instantiate ("Player",SpawnPoint.transform.position, SpawnPoint.transform.rotation, 0);
+		StandbyCamera.SetActive ( false);
+
+		//((MonoBehaviour)MyPly.GetComponent("FPS_Ctrl")).enabled = true;
+		((MonoBehaviour)MyPly.GetComponent("FPSInputController")).enabled = true;
+		((MonoBehaviour)MyPly.GetComponent("MouseLook")).enabled = true;
+		((MonoBehaviour)MyPly.GetComponent("CharacterMotor")).enabled = true;
+		MyPly.transform.FindChild ("Main Camera").gameObject.SetActive( true);
+	}
+}
