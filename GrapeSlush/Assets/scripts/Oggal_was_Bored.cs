df8f183b95239140b190b8b4b1d31b1cfb57d1ce
Oggal <legoboy1122@yahoo.com>
2014-08-01 13:17:03 -0500

    Removed old scene, played around some
    Remember to remove/reactivate Oggal_was_bored

---
+++ GrapeSlush/Assets/scripts/Oggal_was_Bored.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Oggal_was_Bored : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Oggal was Bored is still in the game!!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//lets see what we can do here...
+		if (PhotonNetwork.inRoom && PhotonNetwork.countOfPlayers >= 2) {
+			Debug.Log("You are in room " + PhotonNetwork.room.ToString() + "!");		
+		}
+	}
+}
